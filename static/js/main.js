574496ec160f62843c7d224433cccdf1cd8dc733
Shantanu Sharma <shantanusharma197@gmail.com>
2021-10-13 22:53:27 +0530

    JS for recommendations

--- static/js/main.js
+++ static/js/main.js
@@ -2,51 +2,16 @@
 $(document).ready(function(){
   
     // This function automatically uses the returned json from backend and shows list, but 
-    // We just have to make sure whatever we are trying to return will be in in  `value` key
+    // We just have to make sure whatever we are trying to return will be in `value` key
     $("#autocomplete-input").autocomplete({
       source: "/movies/get_movies/",
-      // select: function (event, ui) { //item selected
-      //   console.log(ui.item);
-      //   AutoCompleteSelectHandler(event, ui)
-      // },
       minLength: 2,
       select: function (event, ui) {
         $("#movieID").val(ui.item.id);
-        // console.log($("#hide1").val());
       }
 
     });
 
-  // Read: https://tarekraafat.github.io/autoComplete.js/#/usage?id=_2-script
-  // const autoCompleteJS = new autoComplete({
-  //   selector: "#autoComplete",
-  //   placeHolder: "Search for Movie...",
-  //   data: {
-  //       src: movie_titles,
-  //       // cache: true,
-  //   },
-  //   resultsList: {
-  //       element: (list, data) => {
-  //           if (!data.results.length) {
-  //               // Create "No Results" message element
-  //               const message = document.createElement("div");
-  //               // Add class to the created element
-  //               message.setAttribute("class", "no_result");
-  //               // Add message text content
-  //               message.innerHTML = `<span>Found No Results for "${data.query}"</span>`;
-  //               // Append message element to the results list
-  //               list.prepend(message);
-  //           }
-  //       },
-  //       noResults: true,
-  //   },
-  //   resultItem: {
-  //       highlight: {
-  //           render: true
-  //       }
-  //   }
-  // });
-  
 
   // Get input field
 	const inputField = document.getElementById('autocomplete-input');  // Or we can use: document.querySelector() 
@@ -66,40 +31,53 @@ $(document).ready(function(){
 
 
   // When Search button is clicked
-  $('#search_button').on('click', function() {
+  $('#search_button').on('click', function(event) {
   	// First check input field is empty or not
   	const inputField = document.getElementById('autocomplete-input');
   	if(inputField.value == "" ) {
   		return;
   	}
-    var api_key = '7c12036c11aea87af9d938d331e8c107';
-    var title = $('.movie').val();
-    if (title=="") {
+    // event.preventDefault();
+    var movieID = $('#movieID').val();
+    console.log(movieID);
+    if (movieID=="") {
       $('.results').css('display','none');
-      $('.fail').css('display','block');
+      $('.wrong_movie').css('display','block');
+      $('#autocomplete-input').val('');
+      swal("Selected movie is not in our database", "Please select a movie from dropdown!", "warning");
     }
     else {
-      get_movie_details(title);
+      get_movie_details(movieID);
     }
   });
+});
 
-  function get_movie_details(movie_title) {
-    // https://api.themoviedb.org/3/find/tt0111161?api_key=7c12036c11aea87af9d938d331e8c107&external_source=imdb_id
-    // https://api.themoviedb.org/3/movie/tt0111161?api_key=7c12036c11aea87af9d938d331e8c107
-    // https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg
-    const url = '/recommend/?movie=' + movie_title;
+
+// Get movie related info and recommended movies for given movie Id
+function get_movie_details(movie_id) {
+    const url = '/movies/recommend/?movie=' + movie_id;
+    $("#loading").fadeIn();
     $.ajax({
       type:'GET',
       url: url,
-      success: function(movie_details){
-        show_details(movie_details,arr,movie_title,my_api_key,movie_id);
+      dataType: 'html',
+      success: function(response){
+        // When results are sucessfully fetched then display it
+        $('.results').html(response);
+        $('#autocomplete-input').val('');
+        // Remove loader
+        $("#loading").delay(500).fadeOut();
+        $(window).scrollTop(0);
+        $('#search_button').attr('disabled', true);
       },
       error: function(){
-        alert("API Error!");
-        $("#loader").delay(500).fadeOut();
+        // Remove loader
+        $("#loading").fadeOut();
+        // Make input box empty
+        $('#autocomplete-input').val('');
+        $('#movieID').val('');
+        // Display alert 
+        swal("Internal Server Error!", "Cannot fetch results right now", "error");
       },
     });
 }
-
-});
-
